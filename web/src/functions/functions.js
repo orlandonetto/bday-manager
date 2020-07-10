c65c5f2def11e19350309013254fb421b640fd2a
orlandonetto <orlandonetto11@gmail.com>
2020-07-10 01:42:22 -0300

    Exemplo de html utilizando arquivo de javascript separado.

--- web/src/functions/functions.js
+++ web/src/functions/functions.js
@@ -1,67 +1,66 @@
-const axios = require('axios');
+function login(request) {
+  return new Promise((resolve, reject) => {
+    if (!request.username || !request.password)
+      return reject({message: "username or password not informed."});
 
-module.exports = {
-  async login(request) {
-    return await new Promise((resolve, reject) => {
-      if (!request.username || !request.password)
-        return reject({message: "username or password not informed."});
+    // Orlando
+    if (request.username === "orlando" && request.password === "123456")
+      return resolve({message: "login success."});
 
-      // Orlando
-      if (request.username === "orlando" && request.password === "123456")
-        return resolve({message: "login success."});
+    // Aline
+    if (request.username === "aline" && request.password === "123456")
+      return resolve({message: "login success."});
 
-      // Aline
-      if (request.username === "aline" && request.password === "123456")
-        return resolve({message: "login success."});
+    return reject({message: "username or password is invalid."});
+  });
+}
 
-      return reject({message: "username or password is invalid."});
-    });
-  },
+function register(request) {
+  return new Promise((resolve, reject) => {
+    return resolve({message: "register success."});
+  });
+}
 
-  async register(request) {
-    return await new Promise((resolve, reject) => {
-      return resolve({message: "register success."});
-    });
-  },
+/* \/ ###### CRUD ###### \/ */
 
-  /* \/ ###### CRUD ###### \/ */
+function createBirthday(request) {
+  return new Promise((resolve, reject) => {
+    return resolve({message: "create success."});
+  });
+}
 
-  async createBirthday(request) {
-    return await new Promise((resolve, reject) => {
-      return resolve({message: "create success."});
-    });
-  },
-  async readBirthdays(request) {
-    return await new Promise((resolve, reject) => {
-      const birthdays = [
-        {
-          name: 'Aline',
-          date: '16-02-1995',
-          age: '25'
-        },
-        {
-          name: 'Alice',
-          date: '12-03-1996',
-          age: '25'
-        },
-        {
-          name: 'Orlando',
-          date: '15-10-1997',
-          age: '22'
-        },
-      ];
+function getBirthdays(request) {
+  return new Promise((resolve, reject) => {
+    const birthdays = [
+      {
+        name: 'Aline',
+        date: '16-02-1995',
+        age: '25',
+      },
+      {
+        name: 'Alice',
+        date: '12-03-1996',
+        age: '25'
+      },
+      {
+        name: 'Orlando',
+        date: '15-10-1997',
+        age: '22'
+      },
+    ];
 
-      return resolve({message: "get success.", birthdays: birthdays});
-    });
-  },
-  async updateBirthday(request) {
-    return await new Promise((resolve, reject) => {
-      return resolve({message: "update success."});
-    });
-  },
-  async removeBirthday(request) {
-    return await new Promise((resolve, reject) => {
-      return resolve({message: "delete success."});
-    });
-  },
+    return resolve({message: "get success.", birthdays: birthdays});
+  });
+}
+
+function updateBirthday(request) {
+  return new Promise((resolve, reject) => {
+    return resolve({message: "update success."});
+  });
+}
+
+function removeBirthday(request) {
+  return new Promise((resolve, reject) => {
+    return resolve({message: "delete success."});
+  });
 }
