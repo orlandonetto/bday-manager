f55d4cbc6def515e878abcc32900b522a680a5b8
orlandonetto <orlandonetto11@gmail.com>
2020-07-11 13:33:24 -0300

    Login, Register, GetBirthdays And CreateBirthdays added in functions.

--- web/src/functions/functions.js
+++ web/src/functions/functions.js
@@ -1,22 +1,47 @@
+const logins = [
+  {username: "orlando", password: "123456"},
+  {username: "aline", password: "123456"},
+];
+const birthdays = [
+  {
+    name: 'Aline',
+    date: '16-02-1995',
+    age: '25',
+  },
+  {
+    name: 'Alice',
+    date: '12-03-1996',
+    age: '25'
+  },
+  {
+    name: 'Orlando',
+    date: '15-10-1997',
+    age: '22'
+  },
+];
+
 function login(request) {
   return new Promise((resolve, reject) => {
     if (!request.username || !request.password)
       return reject({message: "username or password not informed."});
 
-    // Orlando
-    if (request.username === "orlando" && request.password === "123456")
-      return resolve({message: "login success."});
+    const auth = logins.filter(e => e.username === request.username && e.password === request.password);
 
-    // Aline
-    if (request.username === "aline" && request.password === "123456")
-      return resolve({message: "login success."});
+    if (auth.length < 1)
+      return reject({message: "username or password is invalid."});
 
-    return reject({message: "username or password is invalid."});
+    return resolve({message: "Login successfully.", token: "eYa1k2x9", auth: {username: auth[0].username}})
   });
 }
 
 function register(request) {
   return new Promise((resolve, reject) => {
+    if (!request.username || !request.password)
+      return reject({message: "username or password not informed."});
+
+    // Inserting
+    logins.push({username: request.username, password: request.password});
+
     return resolve({message: "register success."});
   });
 }
@@ -25,30 +50,22 @@ function register(request) {
 
 function createBirthday(request) {
   return new Promise((resolve, reject) => {
+    if (!request.name || !request.date || !request.age)
+      return reject({message: "'name', 'date' or 'age' not informed."});
+
+    // Check if exists by name
+    if (birthdays.some(e => e.name === request.name))
+      return reject({message: "failed to create a new birthday, because request name already exists."});
+
+    // Inserting
+    birthdays.push({name: request.name, date: request.date, age: request.age},)
+
     return resolve({message: "create success."});
   });
 }
 
 function getBirthdays(request) {
   return new Promise((resolve, reject) => {
-    const birthdays = [
-      {
-        name: 'Aline',
-        date: '16-02-1995',
-        age: '25',
-      },
-      {
-        name: 'Alice',
-        date: '12-03-1996',
-        age: '25'
-      },
-      {
-        name: 'Orlando',
-        date: '15-10-1997',
-        age: '22'
-      },
-    ];
-
     return resolve({message: "get success.", birthdays: birthdays});
   });
 }
